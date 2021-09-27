58fb40900f12f5640806ffc50a4eae2a9f923708
Alice Genkin <agenkin1@gmail.com>
2021-09-27 17:22:53 -0400

    commit tree 7d64a65351d58efdd73c0c549ddecb12ddecfa90

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(src, callback) {
+  const r = [];
+
+  for (let i = 0; i <src.length; i++) {
+    const elem = src[i]
+    r.push(callback(elem))
+  }
+  return r
+}
+
+function reduce(src,callback) {
+for (let i = 0; i <src.length; i++) {
+  const elem = src[i]
+}
+return src + elem 
+}
